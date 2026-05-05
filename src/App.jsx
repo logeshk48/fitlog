633214b05a82daf0b2cb8bc0dde2f9dc8acdcf75
Logesh K <logeshlogesh093@gmail.com>
2026-05-05 21:56:07 +0530

    🔧 Fix: HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Workouts from './pages/Workouts/Workouts'
@@ -9,7 +9,7 @@ import Profile from './pages/Profile/Profile'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="profile" element={<Profile />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
